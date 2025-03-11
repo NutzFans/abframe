1f9bb1a5d6f32eea0710ecb47a5b2f76e0776f71
recallcode <recallcode@aliyun.com>
2025-03-11 16:49:15 +0800

    合同管理 - 付费合同流程 - 审批节点 ‘承办部门项监部负责人审批’、‘部门领导审批’、‘项监部负责人审批’。

--- com.zhonghe.ame.purchase/src/com/zhonghe/ame/purchase/OrgUtils.java
+++ com.zhonghe.ame.purchase/src/com/zhonghe/ame/purchase/OrgUtils.java
@@ -1,9 +1,12 @@
 package com.zhonghe.ame.purchase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.db.Entity;
 import cn.hutool.db.Session;
@@ -86,6 +89,42 @@ public class OrgUtils {
 			return null;
 		} catch (Exception e) {
 			return null;
+		}
+	}
+
+	@Bizlet("获取项监部负责人、部门负责人 - 默认组织机构部门负责人，但分公司项监部有特殊逻辑处理")
+	public HashMap<String, String> getProjectOrgManager(int orgId) {
+		HashMap<String, String> person = new HashMap<String, String>();
+		try {
+			Session dbSession = new Session(DataSourceHelper.getDataSource());
+			String queryOrgSqlOne = "SELECT ORGLEVEL, ORGSEQ, JUDGE, MANAGERID FROM OM_ORGANIZATION WHERE ORGID = ?";
+			String queryEmpSqlOne = "SELECT EMPCODE, EMPNAME FROM OM_EMPLOYEE WHERE EMPID = ?";
+			Entity orgEntity = dbSession.queryOne(queryOrgSqlOne, orgId);
+			String orgLevel = orgEntity.getStr("ORGLEVEL");
+			if (NumberUtil.parseInt(orgLevel) > NumberUtil.parseInt("3")) {
+				String orgSeq = orgEntity.getStr("ORGSEQ");
+				String[] splitToArray = StrUtil.splitToArray(orgSeq, ".");
+				String secDepOrgId = splitToArray[3];
+				Entity secDepOrgEntity = dbSession.queryOne(queryOrgSqlOne, secDepOrgId);
+				// 直接使用二级部门组织的机构负责人
+				if (StrUtil.isNotBlank(secDepOrgEntity.getStr("MANAGERID"))) {
+					Entity empEntity = dbSession.queryOne(queryEmpSqlOne, secDepOrgEntity.getInt("MANAGERID"));
+					person.put("empCode", empEntity.getStr("EMPCODE"));
+					person.put("empName", empEntity.getStr("EMPNAME"));
+					person.put("type", "person");
+					return person;
+				}
+			}
+			// 直接使用当前组织的机构负责人
+			if (StrUtil.isNotBlank(orgEntity.getStr("MANAGERID"))) {
+				Entity empEntity = dbSession.queryOne(queryEmpSqlOne, orgEntity.getInt("MANAGERID"));
+				person.put("empCode", empEntity.getStr("EMPCODE"));
+				person.put("empName", empEntity.getStr("EMPNAME"));
+				person.put("type", "person");
+			}
+			return person;
+		} catch (Exception e) {
+			return person;
 		}
 	}
 
